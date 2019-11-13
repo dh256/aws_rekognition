7802e48c553580f37a258ec04f2cda6a3bae2a15
David Hanley <david.hanley12@btinternet.com>
2019-11-13 20:17:01 +0000

    Various updates

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # AWS_Rekognition
 
-Example of using AWS Rekognition to recognise objects in an image.
+Example of using AWS Rekognition (Objects and Scenese) to recognise objects (labels) in an image.
+AWS Rekognition will recognise a number of possible objects and scenes in an image. Each one is returned with a confidence indicator, any parent labels and any instances of the label. Any instances detected will have a Bounding Box of where object is in image.
+
+Call detect_labels and examine return values.
+Can either use byte array (Stock Images) or a S3 bucket (Camera Images) as source of image.
+
+AWS Rekognition documentation: https://docs.aws.amazon.com/rekognition/
+Pricing: https://aws.amazon.com/rekognition/pricing/
--- rekognition.py
+++ rekognition.py
@@ -1,17 +1,24 @@
 import boto3
 import time
+import json
 
-def process_image_file_local(filename):
-    with open(filename, 'rb') as fileinput:
-        image = fileinput.read()
-    client = boto3.client('rekognition')
-    start_time = time.time()
-    response = client.detect_labels(Image={'Bytes':image})
-    end_time = time.time()
-    for label in response['Labels']:
-        if label['Name'] == 'Fox':
-            print(f"{label['Name']}: {label['Confidence']}")
-    print(f'Time taken: {end_time - start_time}')
+def process_image_file_local(filename,min_confidence):
+    try:
+        with open(filename, 'rb') as fileinput:
+            image = fileinput.read()
+        client = boto3.client('rekognition')
+        start_time = time.time()
+        response = client.detect_labels(Image={'Bytes':image}, MinConfidence=min_confidence)
+        end_time = time.time()
+        labels = []
+        for label in response['Labels']:
+            if label['Name'] == 'Fox' or label['Name'] == 'Cat' or label['Name'] == 'Dog':
+                labels.append({'Name':label['Name'],'Confidence':label['Confidence']})
+        time_taken = end_time - start_time
+        return {'image':photo,'labels':labels,'time':time_taken}
+    except Exception as e:
+        # print(e)
+        return None
 
 def process_image_file_s3(bucket, photo, min_confidence):
     client = boto3.client('rekognition')
@@ -25,20 +32,35 @@ def process_image_file_s3(bucket, photo, min_confidence):
             if label['Name'] == 'Fox' or label['Name'] == 'Cat' or label['Name'] == 'Dog':
                 labels.append({'Name':label['Name'],'Confidence':label['Confidence']})
         return {'image':photo,'labels':labels,'time':time_taken}
-    except:
+    except Exception as e:
+        # print(e)
         return None
 
 def process_video_file_s3():
     pass
 
-min_confidence = 10
-bucket = 'foxcat-images'
-photos = [f'Snapshot{x:06d}.png' for x in range(1,41)]
-for photo in photos:
-    result = process_image_file_s3(bucket,photo,min_confidence)
-    if result is not None:
-        label_str = ""
-        for label in result['labels']:
-            label_str += f"{label['Name']} ({label['Confidence']:.2f});"
-        print(f"{result['image']}: {label_str}")
-
+in_val = input('Stock (S) or Camera (C): ')
+min_confidence = 50
+if in_val in ('S', 's'):
+    # local
+    photos = [f'stock_images/fox{x}.jpg' for x in range(1,6)]
+    for photo in photos:
+        result = process_image_file_local(photo,min_confidence)
+        if result is not None:
+            label_str = ""
+            for label in result['labels']:
+                label_str += f"{label['Name']} ({label['Confidence']:.2f});"
+            print(f"{result['image']}: {label_str} in {result['time']}s")
+elif in_val in ('C', 'c'):
+    # s3
+    bucket = 'foxcat-images'
+    photos = [f'Snapshot{x:06d}.png' for x in range(1,41)]
+    for photo in photos:
+        result = process_image_file_s3(bucket,photo,min_confidence)
+        if result is not None:
+            label_str = ""
+            for label in result['labels']:
+                label_str += f"{label['Name']} ({label['Confidence']:.2f});"
+            print(f"{result['image']}: {label_str} in {result['time']}s")
+else:
+    print('Nothing to see here!') 
